ec0765770ab9e64352ea809e2762f8e65bdc3b46
“Vencelg” <vaclav.gazda@b2a.cz>
2022-12-19 20:56:50 +0100

    [Fix] change default factory images

--- database/factories/ListingImageFactory.php
+++ database/factories/ListingImageFactory.php
@@ -19,11 +19,18 @@ class ListingImageFactory extends Factory
     {
         $id = fake()->numberBetween(1, 25);
         $listing = Listing::find($id);
+        $images = [
+            'https://g.denik.cz/1/f7/fiat-multipla-2002-1280-03_denik-630-16x9.jpg',
+            'https://mf.b37mrtl.ru/rbthmedia/images/2021.05/original/60b4cf3985600a50dd341bb7.jpg',
+            'https://www.thesprucepets.com/thmb/7TDhfkK5CAKBWEaJfez6607J48Y=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chinese-dog-breeds-4797219-hero-2a1e9c5ed2c54d00aef75b05c5db399c.jpg',
+            'https://cdn.shopify.com/s/files/1/0053/8348/7570/products/foam-latex-witches-nose-prosthetics-alternative-view1_2000x_268139c6-9e68-497b-975b-8191eb894eec_900x.jpg?v=1641315675',
+            'https://im9.cz/iR/importprodukt-orig/500/500c2faf8125d023cbb3160516b3f92f--mmf400x400.jpg'
+        ];
 
         return [
             'listing_id' => $listing->id,
             'name' => str_replace(' ', '_', $listing->name) . '_' . $listing->id . '_' . time() . '_image.jpg',
-            'url' => 'https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/146492478_2916079318663724_3689580725300942431_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=NXidcPGAwLwAX_ZTJOa&_nc_ht=scontent-prg1-1.xx&oh=00_AfBzHHIr1OjuDDoe2b8g1oJlWeJBtMtuP3HV_1MBUA85ag&oe=63C56954'
+            'url' => $images[rand(1, count($images))]
         ];
     }
 }
